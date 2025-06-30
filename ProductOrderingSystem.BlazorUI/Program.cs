a2a940b0448d5aa71233b7382da04d654be4601b
Fatih Güven <fatih.guven@lcwaikiki.com>
2025-06-30 06:00:33 +0300

    fix v1

--- ProductOrderingSystem.BlazorUI/Program.cs
+++ ProductOrderingSystem.BlazorUI/Program.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProductOrderingSystem.BlazorUI;
 using ProductOrderingSystem.BlazorUI.Services;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7076/") });
+var apiBaseUrl = builder.Configuration["ApiEndpoints:BaseUrl"];
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(apiBaseUrl!)
+});
+
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- ProductOrderingSystem.Infrastructure/FakeStoreClient/FakeStoreClient.cs
+++ ProductOrderingSystem.Infrastructure/FakeStoreClient/FakeStoreClient.cs
@@ -1,19 +1,22 @@
-﻿using System.Net.Http.Json;
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
 
 namespace ProductOrderingSystem.Infrastructure.FakeStoreClient;
 
 public class FakeStoreClient
 {
     private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
 
-    public FakeStoreClient(HttpClient httpClient)
+    public FakeStoreClient(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
+        _baseUrl = configuration["ExternalServices:FakeStoreApiUrl"];
     }
 
     public async Task<List<FakeStoreProductDto>> GetProductsAsync()
     {
-        var products = await _httpClient.GetFromJsonAsync<List<FakeStoreProductDto>>("https://fakestoreapi.com/products");
+        var products = await _httpClient.GetFromJsonAsync<List<FakeStoreProductDto>>($"{_baseUrl}/products");
         return products ?? new List<FakeStoreProductDto>();
     }
 }
